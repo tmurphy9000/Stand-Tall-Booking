941b25baaa3959a44fcb4d6b69707e0697ef6cd8
Tanner Murphy <tmurphy9000@gmail.com>
2026-03-18 01:50:12 +0000

    File changes

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -268,6 +268,7 @@ export default function CalendarPage() {
         barbers={activeBarbers}
         services={services}
         prefill={bookingPrefill}
+        bookings={bookings}
       />
 
       <BookingFormModal
@@ -277,6 +278,7 @@ export default function CalendarPage() {
         barbers={activeBarbers}
         services={services}
         prefill={bookingPrefill}
+        bookings={bookings}
       />
 
       <BookingContextMenu
