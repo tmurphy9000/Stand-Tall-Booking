829f499bd617dc386bb64f4100095b48b24e158b
Tanner Murphy <tmurphy9000@gmail.com>
2026-03-18 02:21:24 +0000

    File changes

--- src/components/settings/BarberManager.jsx
+++ src/components/settings/BarberManager.jsx
@@ -4,7 +4,6 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/dialog";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Switch } from "@/components/ui/switch";
 import { Plus, Pencil, Clock, Camera, Trash2, CheckCircle, XCircle, Timer } from "lucide-react";
 import BarberHoursEditor from "./BarberHoursEditor";
