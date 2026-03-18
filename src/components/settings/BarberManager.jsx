c370e910a0fb3db5003cfa9bb37ea529be82214b
Tanner Murphy <tmurphy9000@gmail.com>
2026-03-18 02:29:57 +0000

    File changes

--- src/components/settings/BarberManager.jsx
+++ src/components/settings/BarberManager.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { base44 } from "@/api/base44Client";
-import { useQuery } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
